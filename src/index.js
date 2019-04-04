641af4c974d90f0debeef43a4621676bc9327389
madani6 <anandamadani06@gmail.com>
2019-04-04 16:38:24 +0700

    delete folder and file

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-// import Counters from './components/counters';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
